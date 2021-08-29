facf3535690c93632bc34c501392860a7e111dfb
Aya Oki <countrylife.uk@gmail.com>
2021-08-29 21:31:41 +0900

    ファイルアップロード処理を実装

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/ayacai115/gqlgen-file-upload
 go 1.16
 
 require (
-	github.com/99designs/gqlgen v0.13.0 // indirect
-	github.com/vektah/gqlparser/v2 v2.1.0 // indirect
+	github.com/99designs/gqlgen v0.13.0
+	github.com/vektah/gqlparser/v2 v2.1.0
+	golang.org/x/text v0.3.0
 )
--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -5,14 +5,41 @@ package graph
 
 import (
 	"context"
+	"encoding/csv"
 	"fmt"
+	"io"
 
 	"github.com/ayacai115/gqlgen-file-upload/graph/generated"
 	"github.com/ayacai115/gqlgen-file-upload/graph/model"
 )
 
 func (r *mutationResolver) UploadCsv(ctx context.Context, input model.UploadCsvInput) (*model.UploadCsvPayload, error) {
-	panic(fmt.Errorf("not implemented"))
+	// ShiftJISで読み込むコンバータを用意
+	//converter := transform.NewReader(input.CsvData.File, japanese.ShiftJIS.NewDecoder())
+	// csvのreaderを作成
+	csvReader := csv.NewReader(input.CsvData.File) // UTF-8なら converterの代わりにinput.CsvData.Fileを直で入れる
+
+	// 1行目はヘッダなのでスキップ
+	_, err := csvReader.Read()
+	if err != nil {
+		panic("failed to read first csv")
+	}
+
+	for index := 0; ; index++ {
+		row, err := csvReader.Read()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			panic("failed to read csv")
+		}
+
+		fmt.Println(fmt.Sprintf("%s: %s", row[0], row[1]))
+	}
+
+	return &model.UploadCsvPayload{
+		Result: model.UploadResultSucceeded,
+	}, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
